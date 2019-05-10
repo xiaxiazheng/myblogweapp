496329e75c35844a2de1ea9dfec2a9c20049b1cd
zyb <13075218875@163.com>
2019-05-10 17:57:47 +0800

    树基本搞定了

--- src/client/treeHelper.ts
+++
@@ -1,8 +0,0 @@
-// import { baseUrl, axiosGetHelper, axiosPostHelper } from './clientHelper';
-
-// export namespace TreeHelper {
-//   export async function getTree(): Promise<any[]> {
-//     const data = await axiosGetHelper(`${baseUrl}/tree?type=home`);
-//     return data && data.resultsCode === 'success' ? data.data : [];
-//   }
-// }
